9c39411f3d4fc00e537629df1f0034b6ffe95808
linpeilie <1912599989@qq.com>
2017-09-10 19:33:55 +0800

    使用Intellij创建Maven多项目，搭建基本架构

---
+++ onestore-manager/onestore-dao/src/main/java/com/onestore/App.java
@@ -0,0 +1,13 @@
+package com.onestore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
